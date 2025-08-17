6216ccccf93bf9aca6a48fd00c3b624145bb0dc4
Rishabh Singh <20rishabh05@gmail.com>
2025-08-17 22:12:42 +0530

    first commit

--- lecture1/script.js
+++ lecture1/script.js
@@ -2,3 +2,29 @@
 
 var arr = [1, 2, 3, 5]
 console.log(arr);
+
+var a = arr.map(function(item) {
+    return 3;
+})
+
+console.log(a);
+
+
+var b  = {
+    name: "John",
+    age: 30,
+    city: "New York"    
+}
+
+console.log(b.name);
+
+
+async function fetchData() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let data = await response.json();
+    console.log(data.userId);
+    console.log("hello");
+    
+}
+
+fetchData().userId
