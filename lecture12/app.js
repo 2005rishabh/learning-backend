664aac08a12435f46a1abd5dcb51821a9f708051
Rishabh Singh <20rishabh05@gmail.com>
2025-08-29 21:20:41 +0530

    added update and delete function

--- lecture12/app.js
+++ lecture12/app.js
@@ -4,26 +4,45 @@ const path = require('path');
 const userModel = require('./models/user');
 
 app.use(exp.static(path.join(__dirname, 'public')));
-app.use(exp.json())
+app.use(exp.json());
 app.use(exp.urlencoded({ extended: true }));
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 
+// Home (Create User Form)
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
 
+// Read Users
 app.get('/read', async (req, res) => {
     let users = await userModel.find();
-    res.render('read', {users});
-})
+    res.render('read', { users });
+});
 
+// Create User
 app.post('/create', async (req, res) => {
-    let {name, email, image} = req.body;
-    let newU = await userModel.create({
-        name, email, image
-    })
+    let { name, email, image } = req.body;
+    await userModel.create({ name, email, image });
+    res.redirect('/read');
+});
 
-    res.send(newU);
-})
+// Delete User
+app.get('/delete/:id', async (req, res) => {
+    await userModel.findByIdAndDelete(req.params.id);
+    res.redirect('/read');
+});
 
-app.listen(3000);
+// Edit User - Show Form
+app.get('/edit/:id', async (req, res) => {
+    let user = await userModel.findById(req.params.id);
+    res.render('edit', { user });
+});
+
+// Edit User - Submit Changes
+app.post('/edit/:id', async (req, res) => {
+    let { name, email, image } = req.body;
+    await userModel.findByIdAndUpdate(req.params.id, { name, email, image });
+    res.redirect('/read');
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
